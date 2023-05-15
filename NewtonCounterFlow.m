947072526223fc54838eaf58faaa594869c32363
Victor Yuzhanin <yuzhanin.v@gubkin.ru>
2023-05-15 12:11:38 +0300

    reafactor: Повышение уровня абстракции при расчете противотока методом Ньютона

--- NewtonCounterFlow.m
+++ NewtonCounterFlow.m
@@ -2,52 +2,27 @@ function NewtonCounterFlow()
 clc;
 close all;
 
-function TOutCold = newtonMethod(func, df, TOutCold0, TInHot, TInColdSet)
-    % функция метода Ньютона
-    % func - функция, которую нужно решить
-    % df - производная функции
-    % TOutCold0 - начальное приближение
-    % TInHot - температура горячего потока на входе
-    % TInColdSet - заданная температура холодного потока на входе
-    eps = 0.001; % допустимая погрешность
-    max_iter = 100; % максимальное количество итераций
-
-    % Задаем начальное значение
-    TOutCold = TOutCold0;
-
-    for i = 1:max_iter
-        % Вычисляем значение функции и ее производной в точке TOutCold
-        [~,TInColdCalc] = func(TInHot, TOutCold);
-        f = TInColdCalc - TInColdSet;
-        dfx = df(TInHot, TOutCold);
-
-        % Обновляем TOutCold с использованием формулы Ньютона
-        dT = -f/dfx;
-        TOutCold = TOutCold + dT;% новое значение переменной
-
-        % Проверяем, достигли ли мы точности
-        if abs(dT) < eps
-            disp(i)
-            break;
-        end
-    end
-end
-
-function dfx = df(TInHot, TOutCold)
-    h = 0.01;  % small step
-    [~ , TInColdCalc1] = CalcEulerCounterFlowCont(TInHot, TOutCold + h);
-    [~ , TInColdCalc2] = CalcEulerCounterFlowCont(TInHot, TOutCold - h);
-    dfx = (TInColdCalc1 - TInColdCalc2) / (2*h);
-end
-
 % Входные параметры
 Thot_in = 173;
 Tcold_out = 166.1;
 
-[Thot_out, Tcold_in] = CalcEulerCounterFlowCont(Thot_in, Tcold_out);
+% Считаем вспомогательную задачу. 
+% Для заданной выходной температуры холодного потока ищем соответствующую
+% входную. Мы тем самым генерируем исходную информацию, то, что при
+% боевом расчете нам изначально известно
+[~, Tcold_in] = CalcEulerCounterFlowCont(Thot_in, Tcold_out);
 
-TInColdSet = Tcold_in; % - заданная температура холодного потока на входе
+% создаем слой абстракции в виде уравнения f(x) = 0
+f = @(x) CalcEulerCounterFlow_Tcold_in(Thot_in, x) - Tcold_in;
 
-TOutCold = newtonMethod(@CalcEulerCounterFlowCont, @df, 150, Thot_in, TInColdSet)
+[Tcold_out_calc, n] = NewtonMethod(f, 150); % должно совпасть с Tcold_out
 
 end
+
+% Вспомогательная функция, вытаскивает только Tcold_in из решения
+% вспомогательной задачи
+function Tcold_in = CalcEulerCounterFlow_Tcold_in(Thot_in, Tcold_out)
+[~, Tcold_in] = CalcEulerCounterFlowCont(Thot_in, Tcold_out);
+
+end
+
